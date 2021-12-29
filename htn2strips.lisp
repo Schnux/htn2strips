3b3b9e99170616ef8604e87c208233591b08cf94
MaxG <maximilian.goetz@stud.uni-bamberg.de>
2021-12-29 19:55:35 +0100

    update htn-file read

--- htn2strips.lisp
+++ htn2strips.lisp
@@ -1,102 +1,89 @@
-;(htn2strips "git/input/domain.hddl" "git/input/problem.hddl")
+;(htn2strips "input/domain.hddl" "input/problem.hddl")
 ;(split "(:init" problem '())
-(defvar domain '())
-(defvar problem '())
-(defvar htn-actions '())
-(defvar strips-actions '())
+;*..* für globle var
+
+(defvar *domain* '())
+(defvar *problem* '())
+(defvar *htn-task* '())
+(defvar *strips-task* '())
+(defvar *htn-method* '())
+(defvar *strips-method* '())
+(defvar *htn-action* '())
+(defvar *strips-action* '())
 
 (defun htn2strips (domain-file problem-file)
 
-  (get-domain-file domain-file)
-  (get-problem-file problem-file))
+  (read-domain-file domain-file)
+  (read-problem-file problem-file)
+  (setq *htn-task* (get-htn *domain* 'is-task))
+  (setq *htn-method* (get-htn *domain* 'is-method))
+  (setq *htn-action* (get-htn *domain* 'is-action)))
 
-(defun get-domain-file (filename)
-  (setq domain '())
+(defun read-domain-file (filename)
+  (setq *domain* '())
   (with-open-file (stream filename)
-    (loop for line = (read-line stream nil)
-          while line
-          collect (setq domain (if (string= "" (string-right-trim '(#\Tab #\Return) (string-left-trim '(#\Tab) line)))
-                                   domain
-                                   (append domain (list (string-right-trim '(#\Tab #\Return) (string-left-trim '(#\Tab) line))))))))
-  '())
+    (setq *domain* (read stream)))
+  *domain*)
 
-(defun get-problem-file (filename)
-  (setq problem '())
+(defun read-problem-file (filename)
+  (setq *problem* '())
   (with-open-file (stream filename)
-    (loop for line = (read-line stream nil)
-          while line
-          collect (setq problem (if (string= "" (string-right-trim '(#\Tab #\Return) (string-left-trim '(#\Tab) line)))
-                                    problem
-                                    (append problem (list (string-right-trim '(#\Tab #\Return) (string-left-trim '(#\Tab) line))))))))
-  '())
+    (setq *problem* (read stream)))
+  *problem*)
 
-(defun find-substrings (substring string)
-  (loop with sub-length = (length substring)
-        for i from 0 to (- (length string) sub-length)
-        when (string= string substring
-                      :start1 i :end1 (+ i sub-length)) return T))
+(defun is-task (input)
+  (eq input ':task))
 
-(defun count-substrings (substring string)
-  (loop with sub-length = (length substring)
-        for i from 0 to (- (length string) sub-length)
-        when (string= string substring
-                      :start1 i :end1 (+ i sub-length))
-        count it))
+(defun is-method (input)
+  (eq input ':method))
 
-(defun split (substring inlist retlist)
-  (defvar i)
-  (setq i 0)
-  (defvar brackets)
-  (setq brackets 0)
-  (defvar templist)
-  (setq templist '())
-  (setq retlist '())
+(defun is-action (input)
+  (eq input ':action))
 
-  (loop for line in inlist do
-          (if (find-substrings substring line)
-              (progn
-               (setq templist (append templist (list line)))
-               (setq i (+ i 1))
-               (setq brackets (+ brackets (- (count-substrings "(" line) (count-substrings ")" line))))
-               ;(print i)
-               (loop for j from 0 to (- (length inlist) i)
-                     while (> brackets 0)
-                     do (setq templist (append templist (list (nth (+ i j) inlist))))
-                        (setq brackets (+ brackets (- (count-substrings "(" (nth (+ i j) inlist)) (count-substrings ")" (nth (+ i j) inlist))))))
-               (setq retlist (append retlist (list templist)))
-               (setq templist '()))
-              ;else
-              (setq i (+ i 1))))
-  retlist)
+(defun get-htn (list key-word)
+  (remove-if-not key-word (cdr list) :key #'first))
 
 
 ;; translating the actions
 (defun translate-actions ()
-  (setq action '())
-  (setq keywords '(:action :parameters :precondition :effect))
-  (setq current-keyword '())
-  (setq htn-actions-copy htn-actions)
+  (let ((action '())
+        (params '())
+        (preconditions '())
+        (postconditions '())
+        (keywords '(:action :parameters :precondition :effect))
+        (current-keyword '())
+        (htn-actions-copy htn-actions)))
   (loop for element in htn-actions-copy do
-          (loop while (not (= element nil))
-                do ((if (eq (first element) (first keywords))
-                        ((setq action (append action (second element)) ;;adds the actionname to the list
-                               (setq element (remove element (first element)) ;; remove the :action keyword from the htn action
-                                     (setq element (remove element (first element))))))) ;; remove the actionname from the htn action
-                   )))
+          ((loop while (not (= element nil))
+                 do ((if (eq (first element) (first keywords))
+                         ((setq action (append action (second element)) ;;adds the actionname to the list
+                                (setq element (remove element (first element)) ;; remove the :action keyword from the htn action
+                                      (setq element (remove element (first element))))))) ;; remove the actionname from the htn action
+
+                     (if (eq (first element) (second keywords))
+                         ((setq current-keyword (second keywords))
+                          (setq element (remove element (first element)))))
+
+                     (if (eq (first element) (third keywords))
+                         ((setq current-keyword (third keywords))
+                          (setq element (remo+ve element (first element)))))
+
+                     (if (eq current-keyword (second keywords))
+                         ((setq params (append params (first element)))
+                          (setq element (remove element (first element)))))
+
+                     (if (eq current-keyword (third keywords))
+                         ((setq preconditions (append preconditions (first element)))
+                          (setq element (remove element (first element)))))
+
+                     (if (eq current-keyword (fourth keywords))
+                         ((setq postconditions (append postconditions (first element)))
+                          (setq element (remove element (first element)))))))
+
+           (setq action (append action params))
+           (setq action (append action preconditions))
+           (setq action (append action postconditions))
+           (setq strips-actions (append strips-actions action))))
+
   (setq htn-actions-copy '())
   strips-actions)
-
-(defun string-to-list (str)
-  (if (not (streamp str))
-      (string-to-list (make-string-input-stream str))
-      (if (listen str)
-          (cons (read str) (string-to-list str))
-          nil)))
-
-(defun concatString (list)
-  (if (listp list)
-      (let ((result ""))
-        (dolist (item list)
-          (if (stringp item)
-              (setq result (concatenate 'string result item))))
-        result)))
