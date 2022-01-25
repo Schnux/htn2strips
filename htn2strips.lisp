21a0235b121db7ac2c99561312a356509f375c8e
MaxG <maximilian.goetz@stud.uni-bamberg.de>
2022-01-25 16:06:25 +0100

    add translate-methods, remove-hyphen update write-file

--- htn2strips.lisp
+++ htn2strips.lisp
@@ -10,6 +10,7 @@
 (defvar *strips-action* '())
 (defvar *htn-init* '())
 (defvar *strips-init* '())
+(defvar *htn-goal* '())
 
 (defun question-reader (stream char)
   (declare (ignore char))
@@ -24,13 +25,15 @@
     (setq *problem* (subst '! 'NOT (read-file problem-file))))
 
   ;get and save tasks, methods and actions in lists
-  (setq *htn-task* (get-from-htn *domain* 'is-task))
-  (setq *htn-method* (get-from-htn *domain* 'is-method))
-  (setq *htn-action* (get-from-htn *domain* 'is-action))
+  (setq *htn-task* (remove-hyphen (get-from-htn *domain* 'is-task)))
+  (setq *htn-method* (remove-hyphen (get-from-htn *domain* 'is-method)))
+  (setq *htn-action* (remove-hyphen (get-from-htn *domain* 'is-action)))
   (setq *htn-init* (cdr (first (get-from-htn *problem* 'is-init))))
 
   ;transfrom htn to strips
   (setq *strips-action* (translate-actions))
+
+  (write-file)
 
   (print "Complete"))
 
@@ -115,6 +118,68 @@
     (setq htn-actions-copy '())
     strips-actions))
 
+
+;; translating the methods
+(defun translate-methods (htn-m)
+  (let ((hmethod '())
+        (params '())
+        (task '())
+        (subtasks '())
+        (keywords '(:method :parameters :task :subtasks))
+        (current-keyword '())
+        (strips-methods '()))
+
+    (loop for element in htn-m do
+            (loop while (not (eq element NIL))
+                  do (if (eq (first element) (first keywords))
+                         (progn
+                          (setq hmethod (append hmethod (list (second element)))) ;;adds the actionname to the list
+                          (setq element (remove (first element) element)) ;; remove the :action keyword from the htn action
+                          (setq element (remove (first element) element))))
+                     ;; remove the actionname from the htn action
+
+                     (if (eq (first element) (second keywords))
+                         (progn
+                          (setq current-keyword (second keywords))
+                          (setq element (remove (first element) element))))
+
+                     (if (eq (first element) (third keywords))
+                         (progn
+                          (setq current-keyword (third keywords))
+                          (setq element (remove (first element) element))))
+
+                     (if (eq (first element) (fourth keywords))
+                         (progn
+                          (setq current-keyword (fourth keywords))
+                          (setq element (remove (first element) element))))
+
+                     (if (eq current-keyword (second keywords))
+                         (progn
+                          (setq params (append params (first element)))
+                          (setq element (remove (first element) element))))
+
+                     (if (eq current-keyword (third keywords))
+                         (progn
+                          (setq task (append task (first element)))
+                          (setq element (remove (first element) element))))
+
+                     (if (eq current-keyword (fourth keywords))
+                         (progn
+                          (setq subtasks (append subtasks (first element)))
+                          (setq element (remove (first element) element)))))
+
+            (setq hmethod (append hmethod (list (remove-dash '- params))))
+            (setq hmethod (append hmethod (list task)))
+            (setq hmethod (append hmethod (list subtasks)))
+
+            (setq params '())
+            (setq task '())
+            (setq subtasks '())
+            (setq strips-methods (append strips-methods (list hmethod)))
+            (setq hmethod '()))
+
+    strips-methods))
+
 (defun remove-dash (item list)
   (cond ((null list) nil)
         ((equal item (first list))
@@ -122,19 +187,45 @@
         (T (cons (first list)
                  (remove-dash item (rest list))))))
 
+(defun remove-hyphen (h-list)
+  (first (string-to-list (remove #\- (write-to-string h-list)))))
+
+;; from https://stackoverflow.com/questions/7459501/how-to-convert-a-string-to-list-using-clisp/13832673
+(defun string-to-list (str)
+  (if (not (streamp str))
+      (string-to-list (make-string-input-stream str))
+      (if (listen str)
+          (cons (read str) (string-to-list str))
+          nil)))
+
 (defun write-file ()
-  (with-open-file (file #P"output.strips" :direction :output
+  (with-open-file (file #P"output.txt" :direction :output
                         :if-exists :supersede
                         :if-does-not-exist :create)
     (format file "Initial state: ")
+    ;;init
     (let ((i 0))
       (loop for element in *htn-init* do
               (setq i (1+ i))
               (format file "~{~a(~a)~}" element)
               (if (< i (length *htn-init*))
                   (format file ","))))
-
     (fresh-line file)
     (format file "Goal state: ")
     (fresh-line file)
-    (format file "Actions: ")))
+    ;;actions
+    (format file "Actions:")
+    (fresh-line file)
+    (let ((i 0))
+      (loop for element in *strips-action* do
+              (setq i (1+ i))
+              (format file "~12,0T~a " (first element))
+              (format file "(~{~a~^,~})~%" (second element))
+              (fresh-line file)
+              (format file "~12,0TPreconditions: ")
+              (fresh-line file)
+              (format file "~12,0TPostconditions: ")
+              (if (< i (length *htn-init*))
+                  (format file ","))
+              (fresh-line file)
+              (terpri file)))))
