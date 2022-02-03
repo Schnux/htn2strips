cad8bf21c2a91ed04275b447ef9c59cd5327b3fe
MaxG <maximilian.goetz@stud.uni-bamberg.de>
2022-02-03 20:42:27 +0100

    refactor transformation htn to strips

--- htn2strips.lisp
+++ htn2strips.lisp
@@ -10,7 +10,22 @@
 (defvar *strips-action* '())
 (defvar *htn-init* '())
 (defvar *strips-init* '())
-(defvar *htn-goal* '())
+
+(defstruct strips-action
+  name
+  preconditions
+  postconditions
+  parameters)
+
+(defstruct strips-method
+  name
+  task
+  subtasks
+  parameters)
+
+(defstruct strips-task
+  name
+  parameters)
 
 (defun question-reader (stream char)
   (declare (ignore char))
@@ -25,160 +40,72 @@
     (setq *problem* (subst '! 'NOT (read-file problem-file))))
 
   ;get and save tasks, methods and actions in lists
-  (setq *htn-task* (remove-hyphen (get-from-htn *domain* 'is-task)))
-  (setq *htn-method* (remove-hyphen (get-from-htn *domain* 'is-method)))
-  (setq *htn-action* (remove-hyphen (get-from-htn *domain* 'is-action)))
-  (setq *htn-init* (cdr (first (get-from-htn *problem* 'is-init))))
+  (setq *htn-task* (get-from-htn *domain* ':task))
+  (setq *htn-method* (get-from-htn *domain* ':method))
+  (setq *htn-action* (get-from-htn *domain* ':action))
+  (setq *htn-init* (cdr (first (get-from-htn *problem* ':init))))
 
   ;transfrom htn to strips
-  (setq *strips-action* (translate-actions))
+  (setq *strips-action* (parse-actions *htn-action*))
+  (setq *strips-method* (parse-methods *htn-method*))
+  (setq *strips-task* (parse-tasks *htn-task*))
 
-  (write-file)
-
-  (print "Complete"))
+  ;; Geht nicht - Fixen!
+  ;;(setq *strips-action* (de-hyphenate-symbols *strips-action*))
+  ;; Complete doppelt, da auch zurÃ¼ckgegeben und evaluiert
+  "Complete")
 
 (defun read-file (filename)
   (with-open-file (stream filename)
     (read stream)))
 
-(defun is-task (input)
-  (eq input ':task))
-
-(defun is-method (input)
-  (eq input ':method))
-
-(defun is-action (input)
-  (eq input ':action))
-
-(defun is-init (input)
-  (eq input ':init))
+(defun is-of-type (my-type input)
+  (eq input my-type))
 
 (defun get-from-htn (list key-word)
-  (remove-if-not key-word (cdr list) :key #'first))
+  (remove-if-not (lambda (elem) (is-of-type key-word elem)) (cdr list) :key #'first))
 
-;; translating the actions
-(defun translate-actions ()
-  (let ((action '())
-        (params '())
-        (preconditions '())
-        (postconditions '())
-        (keywords '(:action :parameters :precondition :effect))
-        (current-keyword '())
-        (htn-actions-copy *htn-action*)
-        (strips-actions '()))
+;; Fehler?
+(defun remove-hyphen (symbol-or-list)
+  "turns some list (foo-bar baz foo-foo) into (foobar baz foofoo)"
+  ;; (loop for symbol in list-of-symbols collect 
+  ;;   (intern (remove #\- (symbol-name symbol)))
+  (if (listp symbol-or-list)
+      (mapcar #'remove-hyphen symbol-or-list)
+      (intern (remove #\- (symbol-name symbol-or-list)))))
 
-    (loop for element in htn-actions-copy do
-            (loop while (not (eq element NIL))
-                  do (if (eq (first element) (first keywords))
-                         (progn
-                          (setq action (append action (list (second element)))) ;;adds the actionname to the list
-                          (setq element (remove (first element) element)) ;; remove the :action keyword from the htn action
-                          (setq element (remove (first element) element))))
-                     ;; remove the actionname from the htn action
+(defun remove-hyphen-old (h-list)
+  (first (string-to-list (remove #\- (write-to-string h-list)))))
 
-                     (if (eq (first element) (second keywords))
-                         (progn
-                          (setq current-keyword (second keywords))
-                          (setq element (remove (first element) element))))
+(defun parse-actions (htn-actions)
+  (loop for element in htn-actions
+        collect
+        (make-strips-action
+         :parameters (remove-item-and-next '- (parse-it :parameters element))
+         :name (remove-hyphen (second element))
+         :preconditions (remove-hyphen (parse-it :precondition element))
+         :postconditions (remove-hyphen (parse-it :effects element)))))
 
-                     (if (eq (first element) (third keywords))
-                         (progn
-                          (setq current-keyword (third keywords))
-                          (setq element (remove (first element) element))))
+(defun parse-methods (htn-methods)
+  (loop for element in htn-methods
+        collect
+        (make-strips-method
+         :parameters (remove-item-and-next '- (parse-it :parameters element))
+         :name (add-prefix-to-element "M" (list (remove-hyphen (second element))))
+         :task (remove-hyphen (parse-it :task element))
+         :subtasks (remove-hyphen (parse-it :subtasks element)))))
 
-                     (if (eq (first element) (fourth keywords))
-                         (progn
-                          (setq current-keyword (fourth keywords))
-                          (setq element (remove (first element) element))))
+(defun parse-tasks (htn-tasks)
+  (loop for element in htn-tasks
+        collect
+        (make-strips-task
+         :parameters (remove-item-and-next '- (parse-it :parameters element))
+         :name (add-prefix-to-element "T" (list (remove-hyphen (second element)))))))
 
-                     (if (eq current-keyword (second keywords))
-                         (progn
-                          (setq params (append params (first element)))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq current-keyword (third keywords))
-                         (progn
-                          (setq preconditions (append preconditions (first element)))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq current-keyword (fourth keywords))
-                         (progn
-                          (setq postconditions (append postconditions (first element)))
-                          (setq element (remove (first element) element)))))
-
-            (setq action (append action (list (remove-item-and-next '- params))))
-            (setq action (append action (list preconditions)))
-            (setq action (append action (list postconditions)))
-            (setq params '())
-            (setq preconditions '())
-            (setq postconditions '())
-            (setq strips-actions (append strips-actions (list action)))
-            (setq action '()))
-
-    (setq htn-actions-copy '())
-    strips-actions))
-
-
-;; translating the methods
-(defun translate-methods (htn-m)
-  (let ((hmethod '())
-        (params '())
-        (task '())
-        (subtasks '())
-        (keywords '(:method :parameters :task :subtasks))
-        (current-keyword '())
-        (strips-methods '()))
-
-    (loop for element in htn-m do
-            (loop while (not (eq element NIL))
-                  do (if (eq (first element) (first keywords))
-                         (progn
-                          (setq hmethod (append hmethod (list (second element)))) ;;adds the actionname to the list
-                          (setq element (remove (first element) element)) ;; remove the :action keyword from the htn action
-                          (setq element (remove (first element) element))))
-                     ;; remove the actionname from the htn action
-
-                     (if (eq (first element) (second keywords))
-                         (progn
-                          (setq current-keyword (second keywords))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq (first element) (third keywords))
-                         (progn
-                          (setq current-keyword (third keywords))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq (first element) (fourth keywords))
-                         (progn
-                          (setq current-keyword (fourth keywords))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq current-keyword (second keywords))
-                         (progn
-                          (setq params (append params (first element)))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq current-keyword (third keywords))
-                         (progn
-                          (setq task (append task (first element)))
-                          (setq element (remove (first element) element))))
-
-                     (if (eq current-keyword (fourth keywords))
-                         (progn
-                          (setq subtasks (append subtasks (first element)))
-                          (setq element (remove (first element) element)))))
-
-            (setq hmethod (append hmethod (list (remove-item-and-next '- params))))
-            (setq hmethod (append hmethod (list task)))
-            (setq hmethod (append hmethod (list subtasks)))
-
-            (setq params '())
-            (setq task '())
-            (setq subtasks '())
-            (setq strips-methods (append strips-methods (list hmethod)))
-            (setq hmethod '()))
-
-    strips-methods))
+(defun parse-it (keyword element)
+  "grabs keyword definition from (:action foo .... :keyword x y)"
+  ;; todo
+  (second (member keyword element)))
 
 (defun remove-item-and-next (item list)
   (cond ((null list) nil)
@@ -186,17 +113,6 @@
          (remove-item-and-next item (cdr (rest list))))
         (T (cons (first list)
                  (remove-item-and-next item (rest list))))))
-
-(defun remove-hyphen (h-list)
-  (first (string-to-list (remove #\- (write-to-string h-list)))))
-
-;; from https://stackoverflow.com/questions/7459501/how-to-convert-a-string-to-list-using-clisp/13832673
-(defun string-to-list (str)
-  (if (not (streamp str))
-      (string-to-list (make-string-input-stream str))
-      (if (listen str)
-          (cons (read str) (string-to-list str))
-          nil)))
 
 (defun write-file ()
   (with-open-file (file #P"output.txt" :direction :output
@@ -229,3 +145,16 @@
                   (format file ","))
               (fresh-line file)
               (terpri file)))))
+
+(defun add-prefix-to-element (prefix element)
+  (first (string-to-list (concatenate 'String prefix (write-to-string (first element))))))
+
+
+;; https://stackoverflow.com/questions/7459501/how-to-convert-a-string-to-list-using-clisp/13832673
+;Turns a string into a stream so it can be read into a list
+(defun string-to-list (str)
+  (if (not (streamp str))
+      (string-to-list (make-string-input-stream str))
+      (if (listen str)
+          (cons (read str) (string-to-list str))
+          nil)))
